8ff3bfb1762e6896774c24dd25f90c59c1a8f4f1
Nicolas Marini <nmarini44@gmail.com>
2019-06-27 00:02:34 +0000

    Automatically backed up by Learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,6 +3,11 @@ class ProductsController < ApplicationController
     @products = Product.all
   end
 
+  def data
+    post = Post.find(params[:id])
+    render json: PostSerializer.serialize(post)
+  end 
+
   def inventory
     product = Product.find(params[:id])
     render plain: product.inventory > 0 ? true : false
