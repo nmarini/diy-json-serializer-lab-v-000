9b26e3572ea6d8040fb800b8d0df2689ff1c945d
Nicolas Marini <nmarini44@gmail.com>
2019-06-27 00:10:49 +0000

    Automatically backed up by Learn

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -3,12 +3,10 @@ class ProductSerializer
     serialized_product = '{'
 
     serialized_product += '"id": ' + product.id.to_s + ', '
-    serialized_product += '"title": "' + product.title + '", '
+    serialized_product += '"name": "' + product.name + '", '
     serialized_product += '"description": "' + product.description + '", '
-
-    # the author association can also be represented in JSON
-    serialized_product += '"author": {'
-    serialized_product += '"name": "' + product.author.name + '"}'
+    serialized_product += '"price": ' + product.price.to_s + ', '
+    serialized_product += '"inventory": "' + product.inventory
 
     # and end with the close brace
     serialized_product += '}'
